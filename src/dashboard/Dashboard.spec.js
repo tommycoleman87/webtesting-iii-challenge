05c44598c34619303c3598cd492a546c9d24d26b
Tommy Coleman <tommiilee@gmail.com>
2019-08-19 16:18:19 -0400

    wrote test to check if dashboard renders correctly

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+describe('<Dashboard />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+})
